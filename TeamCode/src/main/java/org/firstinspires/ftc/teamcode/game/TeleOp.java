73f75f8682ff0ccec43524d45f9890f8faf120d9
jayant <jbhaskaruni@gmail.com>
2023-10-07 19:42:23 -0500

    Made progress on Hang

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/game/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/game/TeleOp.java
@@ -3,11 +3,12 @@ package org.firstinspires.ftc.teamcode.game;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.drive.MecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.Hang;
 
 public class TeleOp extends LinearOpMode {
 
     MecanumDrive mecanumDrive;
-
+    Hang hang = new Hang(hardwareMap);
     @Override
     public void runOpMode() throws InterruptedException {
 
@@ -21,7 +22,13 @@ public class TeleOp extends LinearOpMode {
 
         while (opModeIsActive()) {
 
+            if(gamepad1.a)
+                hang.stateHang(Hang.StateHang.EXTEND);
+            if(gamepad1.b)
+                hang.stateHang(Hang.StateHang.RETRACT);
         }
 
     }
+
+
 }
